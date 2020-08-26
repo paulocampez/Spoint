baddbad3a0744c78538d806a1d5d04d2e0b95f56
PauloRobertoCampez <paulo.campez@midassolutions.com.br>
2020-08-26 07:52:27 -0300

    implementado automapper

--- server/Spoint/src/Spoint.Api/AutoMapper/DomainToViewModelMappingProfile.cs
+++ server/Spoint/src/Spoint.Api/AutoMapper/DomainToViewModelMappingProfile.cs
@@ -1,4 +1,6 @@
 ﻿using AutoMapper;
+using Customers.Domain;
+using Spoint.Api.Models.Customer;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,5 +10,9 @@ namespace Spoint.Api.AutoMapper
 {
     public class DomainToViewModelMappingProfile : Profile
     {
+        public DomainToViewModelMappingProfile()
+        {
+            CreateMap<Customer, CustomerViewModel>();
+        }
     }
 }
--- server/Spoint/src/Spoint.Api/AutoMapper/ViewModelToDomainMappingProfile.cs
+++ server/Spoint/src/Spoint.Api/AutoMapper/ViewModelToDomainMappingProfile.cs
@@ -1,4 +1,7 @@
 ﻿using AutoMapper;
+using Customers.Commands;
+using Customers.Domain;
+using Spoint.Api.Models.Customer;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,5 +11,10 @@ namespace Spoint.Api.AutoMapper
 {
     public class ViewModelToDomainMappingProfile : Profile
     {
+        public ViewModelToDomainMappingProfile()
+        {
+            CreateMap<CreateCustomerViewModel, CreateCustomerCommand>()
+              .ConstructUsing(c => new CreateCustomerCommand(c.Id, c.Name, c.Register, c.BirthDate, c.Email, c.Phone, c.CreationDate));
+        }
     }
 }
