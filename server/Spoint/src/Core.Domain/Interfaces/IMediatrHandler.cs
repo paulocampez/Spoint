8a4cf2357bbeffb27bc11ac04f2a89661ffb7dfa
PauloRobertoCampez <paulo.campez@midassolutions.com.br>
2020-08-25 21:00:19 -0300

    Montado estrutura api

---
+++ server/Spoint/src/Core.Domain/Interfaces/IMediatrHandler.cs
@@ -0,0 +1,17 @@
+﻿using Core.Domain.Commands;
+using Core.Domain.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Core.Domain.Interfaces
+{
+    public interface IMediatrHandler
+    {
+        Task RaiseEvent<T>(T events, CancellationToken cancellation = default, bool enqueue = false) where T : Event;
+        Task SendCommand<T>(T command, CancellationToken cancellation = default, bool enqueue = false) where T : Command;
+    }
+}
---
+++ server/Spoint/src/Spoint.Api/Models/Customer/CreateCustomerViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Spoint.Api.Models.Customer
+{
+    public class CreateCustomerViewModel
+    {
+        public CreateCustomerViewModel()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "Name is required.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Register number is required.")]
+        [MinLength(11, ErrorMessage = "Register number must be valid.")]
+        [MaxLength(11, ErrorMessage = "Register number must be valid.")]
+        public string Register { get; set; }
+
+        [Required(ErrorMessage = "Birth Date is required.")]
+        public DateTime BirthDate { get; set; }
+
+        [Required(ErrorMessage = "E-mail is required.")]
+        [EmailAddress(ErrorMessage = "E-mail must be valid.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Phone is required.")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        public string Password { get; set; }
+
+        public DateTime CreationDate { get; set; }
+
+    }
+}
